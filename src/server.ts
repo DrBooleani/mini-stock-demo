68e9ceab6283a4b400f49b3b9c16600b051f1108
DrBooleani <notdeyvidsalvatoresemail@gmail.com>
2025-02-21 17:31:51 -0300

    feat: CORS

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import routes from './routes';
 import dotenv from 'dotenv';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const FRONTEND_ORIGINS = process.env.FRONTEND_ORIGINS ? process.env.FRONTEND_ORIGINS.split(',') : ["*"];
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: FRONTEND_ORIGINS,
+  methods: "GET, PUT, POST, DELETE",
+  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
+}));
 
 app.use("/", routes);
 
